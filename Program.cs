d2214bd6e9fc26658c183e7130f1d8644ecffb6c
AnnaT <anna-sm@yandex.ru>
2023-02-06 10:31:08 -0800

    Self work4

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Задача - количество цифр в числе - решена, работает
+
+int DigitCount(int number)
+{
+    int count = 0;
+    while(number != 0)
+    {
+        count++;
+        number /= 10;
+    }
+    return count;
+}
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number of digits is: " + DigitCount(num));
+
+Задача 2 Найти факториал - решена, работает
+int Factorial(int number)
+{
+    int result = 1;
+    for(int i = 1; i <= number; i++)
+        result *= i;
+    return result;
+}
+Console.Write("Enter a number N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Factorial is: " + Factorial(num));
+
+2й вариант решения, без ввода переменной - решено, работает
+int Factorial(int number)
+{
+    int result = 1;
+    while(number > 1)
+    {
+        result *= number;
+        number --;
+    }
+    return result;
+}
+Console.Write("Enter a number N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Factorial is: " + Factorial(num));
